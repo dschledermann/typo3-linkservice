415cae28418bed83d31b374c41bd6b24d9362540
Carsten Jørgensen <carsten@linkfactory.dk>
2014-10-03 13:03:19 +0200

    [TASK] Added documentation for methods.

--- lib/class.tx_linkservice_httpresponse.php
+++ lib/class.tx_linkservice_httpresponse.php
@@ -5,6 +5,13 @@ class tx_linkservice_httpresponse {
     public $location = '';
     public $exception_message = '';
 
+    /**
+     * Set new location for url.
+     * 
+     * @param string $new_location
+     * @param string $old_location
+     * @throws Exception
+     */
     public function setLocation($new_location, $old_location) {
         $new_location = trim($new_location);
         $old_parts = parse_url($old_location);
@@ -34,18 +41,38 @@ class tx_linkservice_httpresponse {
         $this->location = preg_replace('/&amp(;amp)*(%3b|;)/i', '&', $this->location);
     }
 
+    /**
+     * Check if response is permanent redirect.
+     * 
+     * @return boolean
+     */
     public function isPermanentRedirect() {
         return $this->statusCode == 301 && $this->location;
     }
 
+    /**
+     * Check if response is a temporary redirect.
+     *
+     * @return boolean
+     */
     public function isTemporaryRedirect() {
         return $this->location && ($this->statusCode == 302 || $this->statusCode == 303 || $this->statusCode == 307);
     }
 
+    /**
+     * Check if response is unavailable.
+     *
+     * @return boolean
+     */
     public function isUnavailable() {
         return $this->statusCode >= 400 && $this->statusCode <= 499;
     }
 
+    /**
+     * Check if response is an error.
+     * 
+     * @return boolean
+     */
     public function isError() {
         return $this->statusCode >= 500;
     }
