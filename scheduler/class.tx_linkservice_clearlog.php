b590cd0884ecd38f384766e0d9cec95d71c6e93e
Daniel Schledermann <daniel@linkfactory.dk>
2015-05-19 13:18:45 +0200

    [BUGFIX] Make sure the job returns true or backend report errors.

--- scheduler/class.tx_linkservice_clearlog.php
+++ scheduler/class.tx_linkservice_clearlog.php
@@ -7,6 +7,8 @@ class tx_linkservice_clearlog extends tx_scheduler_Task {
     public function execute() {
         $this->extConf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['linkservice']);
         $GLOBALS['TYPO3_DB']->exec_DELETEquery('tx_linkservice_log', 'checktime < '.(time() - 3600 * $this->extConf['log_retention']));
+
+				return true;
     }
 }
 
