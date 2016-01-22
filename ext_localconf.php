23b1036f2ebe434416e4dd48c28748afeb4afade
Daniel Schledermann <daniel@linkfactory.dk>
2016-01-22 08:09:00 +0100

    [BUGFIX] Make the cache-configuration overrideable

--- ext_localconf.php
+++ ext_localconf.php
@@ -18,14 +18,9 @@ $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['tx_linkservice_
 
 // Enable cache for extension.
 if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['linkservice'])) {
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['linkservice'] = array();
+	// Set cache to use string for frontend.
+	$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['linkservice'] = array(
+		'frontend' => 't3lib_cache_frontend_VariableFrontend',
+		'backend' => 't3lib_cache_backend_DbBackend',
+		'options' => array());
 }
-
-// Set cache to use string for frontend.
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['linkservice'] = array(
-    'frontend' => 't3lib_cache_frontend_VariableFrontend',
-    'backend' => 't3lib_cache_backend_DbBackend',
-    'options' => array()
-);
-
-
